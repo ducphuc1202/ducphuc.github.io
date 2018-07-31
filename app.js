0c38950f4d68eb3327f8e9171cc6af054ba85669
ducphuc1202 <38980066+ducphuc1202@users.noreply.github.com>
2018-07-31 17:59:34 +0700

    THAY DOI SU KIEN CLICK

--- app.js
+++ app.js
@@ -33,34 +33,6 @@ document.addEventListener('mouseover', function(evt) {
     if(evt.target.matches('.fa')) {
         evt.target.parentElement.parentElement.classList.add('hover-up');
     }
-//     if(evt.target.matches('.btn-hover')) {
-//         var index = 0;
-//         for(let i = 0; i < btnhovers.length; i++) {
-//             if(btnhovers[i].isSameNode(evt.target)) {
-//                 index = i;
-//                 break;
-//             }  
-//         }
-//         var picture = document.querySelector('.pr');
-//         if(picture.classList.contains('visible')) {
-//             picture.classList.remove('visible');
-//         }
-//          setTimeout(function() {
-//             picture.setAttribute('src', src[index]);
-//             picture.classList.add('visible');
-//          },200);
-//         var info = document.querySelector('.current-info');
-//         if(info.classList.contains('moveleft')) {
-//             info.classList.remove('moveleft');
-//         }
-//         setTimeout(function() {
-//             document.querySelector('.title-name').textContent = nameOfProject[index];
-//             document.querySelector('.language-name').textContent = language[index];
-//             document.querySelector('.tech-name').textContent = tech[index];
-//             document.querySelector('.view').setAttribute('href',link[index]);
-//             info.classList.add('moveleft');
-//         },400);
-//     }
 })
 var btnhovers = document.querySelectorAll('.btn-hover');
 for(let t = 0; t < btnhovers.length; t++) {
@@ -101,21 +73,29 @@ document.addEventListener('mouseout', function(evt) {
 })
 var i = 0;
 
-document.addEventListener('click', function(evt) {
-//     if(evt.target.matches('.menu-icon') || evt.target.matches('.fa-bars')) {
-//         document.querySelector('.list').classList.toggle('hidden');
-//     }
-    if(evt.target.matches('.btn-goup') || evt.target.matches('.fa-chevron-up')) {
-        window.scroll( {
+
+/******************************************************************/
+var btngoup = document.querySelector('.btn-goup');
+btngoup.addEventListener('click', function(){
+    window.scroll( {
             top: 0,
             behavior: "smooth"
         });
-    }
 })
+/******************************************************************/
+
+
+
+/******************************************************************/
 var menuicon = document.querySelector('.menu-icon');
 menuicon.addEventListener('click', function(evt) {
     document.querySelector('.list').classList.toggle('hidden');
 })
+/******************************************************************/
+
+
+
+/******************************************************************/
 var sendbtn = document.querySelector('#send');
 sendbtn.addEventListener('click', function(evt) {
         var it = document.createElement('i');
@@ -125,13 +105,16 @@ sendbtn.addEventListener('click', function(evt) {
         evt.target.appendChild(it);
         evt.target.classList.add('sended');
 })
+/******************************************************************/
 
+
+
+/******************************************************************/
 var height = window.innerHeight;
 console.log(height);
 var pos = height - 85;
 var goup = document.querySelector('.btn-goup');
     goup.setAttribute('style', 'top:' + pos + 'px');
-
 document.addEventListener('scroll', function(evt) {
     if(window.scrollY >= 500) {
         goup.classList.remove('hidden');
@@ -140,3 +123,4 @@ document.addEventListener('scroll', function(evt) {
         goup.classList.add('hidden');
     }
 })
+/******************************************************************/
